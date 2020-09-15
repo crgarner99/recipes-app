fb30d0af19334f04c183bc6ef4691a39560e54c5
Cameron Garner <cameron.r.garner@gmail.com>
2020-09-14 19:14:37 -0600

    Second Commit

--- src/RecipeService.js
+++ src/RecipeService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3005";
+//const BASE_URL = "http://localhost:3005";
+const BASE_URL = "https://recipe-app-cameron.herokuapp.com";
 //TODO connect to hosting platform
 // const BASE_URL = "URL of Hosting Platform";
 
